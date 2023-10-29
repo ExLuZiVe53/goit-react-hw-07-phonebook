7c07671ae2953f30427b59daed7a89fc4bb5a6e0
ExLuZiVe53 <imstudent27@gmail.com>
2023-10-29 16:35:46 +0100

    added toaster

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,72 +6,71 @@ import { getContacts } from 'redux/selectors.js';
 import { ContactForm } from './ContactForm/ContactForm.jsx';
 import { Filter } from './Filter/Filter.jsx';
 import { ContactList } from './ContactList/ContactList.jsx';
-import{ Toaster } from 'react-hot-toast';
-import  { Radio } from 'react-loader-spinner';
+
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import { Radio } from 'react-loader-spinner';
 export const App = () => {
   const { items, isLoading, error } = useSelector(getContacts);
-  //переніс сюди діспатч виклик контактів, бо не рендерилась із-за умови items.length(39 рядок)
+
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(operation.fetchContacts());
   }, [dispatch]);
 
-  //нотифікашка на лоадінг
-  // Loading.init({
-  //   svgColor: 'fuchsia',
-  //    });
-//   if (isLoading) {
-//     return (
-      
-//     );
-// }
-//  else {
-//     toast.remove();;
-//   }
-
-
-return (
-  <div
-    style={{
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      fontSize: 40,
-      color: '#010101',
-    }}
-  >
-    {error ? (
-      <h3>{error}</h3>
-    ) : (
-      <div>
-        <h1>Phonebook</h1>
-        <ContactForm />
-        {isLoading && !error && (
-          <Radio
-            visible={true}
-            height="280"
-            width="280"
-            ariaLabel="radio-loading"
-            wrapperStyle={{}}
+  return (
+    <div
+    // style={{
+    //   display: 'flex',
+    //   justifyContent: 'center',
+    //   alignItems: 'center',
+    //   fontSize: 40,
+    //   color: '#010101',
+    // }}
+    >
+      {error ? (
+        <h3>{error}</h3>
+      ) : (
+        <div>
+          <h1>Phonebook</h1>
+          <ContactForm />
+          {isLoading && !error && (
+            <Radio
+              visible={true}
+              height="280"
+              width="280"
+              ariaLabel="radio-loading"
+              wrapperStyle={{}}
               wrapperClass="radio-wrapper"
-              
-          />
-        )}
-        {items[0] ? (
-          <>
-            <h2>Contacts</h2>
-            <Filter />
+            />
+          )}
+          {items[0] ? (
+            <>
+              <h2>Contacts</h2>
+              <Filter />
 
-            <ContactList />
-          </>
-        ) : (
-          <h3>Your phonebook is empty</h3>
-        )}
+              <ContactList />
+            </>
+          ) : (
+            <h3>Your phonebook is empty</h3>
+          )}
+        </div>
+      )}
+      <div>
+        <ToastContainer
+          position="top-left"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark"
+        />
       </div>
-    )}
-    <div>
-      <Toaster />
     </div>
-  </div>
-);
+  );
 };
